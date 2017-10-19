30fa2c0604d2a8676741f54ab900d5fe9573b73f
Macarena Belén Garcia Arcija <belugarciaarcija@gmail.com>
2017-10-19 19:52:15 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -3,8 +3,8 @@
 COSAS QUE HACER
 
 -- obtener datos del dataset
-que anio/mes fue el mas caro y cual el mas barato
-promedio anual del precio
+-que anio/mes fue el mas caro y cual el mas barato
+-promedio anual del precio
 un grafico de como varia el precio
 - verificar que los datos ingresados son validos
 CRUD:
@@ -34,7 +34,7 @@ guardar en una base:
 from flask import Flask
 from flask import request
 from flask import render_template
-
+from decimal import Decimal
 import pandas as pd
 import numpy as np
 import datetime
@@ -42,24 +42,29 @@ import datetime
 
 app = Flask(__name__)
 
-'''
-@app.route('/hello/')
-@app.route('/hello/<nombre>')
-def hello(nombre=None):
-	 return render_template('hola.html', nombre=nombre)
-@app.route('/persona', methods=['GET', 'POST'])
-def persona():
-	if request.method == 'POST':
-		return 'POST'
-	else:
-		return 'GET'
-'''
 
 def obtener_datos():
 	#obtiene los valores del archivo csv
 	df = pd.read_csv("precio-promedio-por-litro-de-leche.csv",encoding = "ISO-8859-1",sep=';', header=None)
 	return df
 	
+def lista_precios():
+	#obtiene listado de precios promedios por mes y anio
+	datos=obtener_datos()
+	lista_r=[]
+	lista_p=[]
+	for i in range(1,75):
+		anio=int(datos[2][i])
+		mes=int(datos[3][i])
+		pre=Decimal(datos[6][i].replace(',', '.'))
+		lista_r.insert(0, anio)#anio
+		lista_r.insert(1, mes)#mes
+		lista_r.insert(2, pre)#precio
+		
+		lista_p.insert(i, lista_r.copy())
+		lista_r.clear()
+	return lista_p
+
 @app.route('/datos', methods=['GET', 'POST'])
 def datos():
 	if request.method == 'GET':
@@ -79,8 +84,9 @@ def formulario():
 	if request.method == 'GET':
 		datos=obtener_datos()
 		return render_template('formulario.html')#retorna un template con un formulario
-		
-
+	if  request.method == 'POST':
+		precios=promedio_anual(2016)
+		print (precios) 
 		
 def comparar(lista, comparador):#ver si se puede hacer con un reduce
 	#ejemplo:  reduce(comparar, lista)
@@ -96,11 +102,18 @@ def comparar(lista, comparador):#ver si se puede hacer con un reduce
 		#error el comparador no es valido
 		pass
 
-def promedio_anual(meses):
+def promedio_anual(anio):
 	#calcula el promedio anual
-	pass
-	#primero se fija si hay 12 elementos en la lista, si no hay devuelve un error
-	#suma los meses y despues los divide por 12
+	#usa lista precio
+	lista_t_p=lista_precios()#listado de todos los precios registrados
+	t_pre=0#total de precio
+	for i in range(1,74):
+		a=lista_t_p[i][0]
+		pre=lista_t_p[i][2]
+		if anio==a:
+			t_pre+=pre
+	return t_pre/12
+	
 
 def verificar_datos(datos):
 	#verifica que los datos ingresados sean validos
@@ -120,6 +133,6 @@ def verificar_datos(datos):
 		return 'El mes ingresado no es valido'
 		
 
-def graficar(lista):
+def graficar(datos):
 	#grafica como varia el precio. Puede ser tanto anual como periodicamente(?????????)
 	pass
