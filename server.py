c409cd33501d7e16b017b4d1116c9f398db95256
Macarena Belén Garcia Arcija <belugarciaarcija@gmail.com>
2017-10-19 22:12:37 -0300

    .-.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 COSAS QUE HACER
 
 -- obtener datos del dataset
-- que anio/mes fue el mas caro y cual el mas barato
+-- que mes fue el mas caro y cual el mas barato
 -- promedio anual del precio
 un grafico de como varia el precio
 - verificar que los datos ingresados son validos
@@ -15,7 +15,7 @@ CRUD:
 
 
 ordenar los datos por anio y mes
-buscar todos los datos de un anio()
+-- buscar todos los datos de un anio()
 comparar anualmente en x mes(ejemplo: cuanto valio en el mes 2 en todos los anios)
 
 
@@ -47,13 +47,35 @@ def obtener_datos():
 	#obtiene los valores del archivo csv
 	df = pd.read_csv("precio-promedio-por-litro-de-leche.csv",encoding = "ISO-8859-1",sep=';', header=None)
 	return df
+
+def obtener_datos_por_anio(anio):
+	lista_t_p=lista_precios()#listado de todos los precios registrados
+	lista_r=[]
+	lista_p=[]
+	j=len(lista_t_p)
 	
+	for i in range(1,j):
+		a=lista_t_p[i][0]
+		if anio==a:
+			m=lista_t_p[i][1]
+			pre=lista_t_p[i][2]
+			lista_r.insert(0, a)#anio
+			lista_r.insert(1, m)#mes
+			lista_r.insert(2, pre)#precio		
+			lista_p.insert(i, lista_r.copy())
+			lista_r.clear()
+	return lista_p
+
+	
+	
+		
 def lista_precios():
 	#obtiene listado de precios promedios por mes y anio
 	datos=obtener_datos()
 	lista_r=[]
 	lista_p=[]
-	for i in range(1,75):
+	j=len(datos)
+	for i in range(1,j):
 		anio=int(datos[2][i])
 		mes=int(datos[3][i])
 		pre=Decimal(datos[6][i].replace(',', '.'))
@@ -84,35 +106,52 @@ def formulario():
 	if request.method == 'GET':
 		datos=obtener_datos()
 		return render_template('formulario.html')#retorna un template con un formulario
-	if  request.method == 'POST':
-		precios=promedio_anual(2016)
+	if request.method == 'POST':
+		#precios=promedio_anual(2016)
+		precios=comparar(2016,'menor')
 		print (precios) 
 		
-def comparar(lista, comparador):#ver si se puede hacer con un reduce
-	#ejemplo:  reduce(comparar, lista)
-	
+def comparar(anio,comparador):
 	#compara cual mes/anio es el menor o mayor
+	
+	pre_m=[]
+	pre_m.insert(0,0)
+	pre_m.insert(1,0)
+	pre_m.insert(2,0)
+	
+	lista_r=[]
+	lista=obtener_datos_por_anio(anio)
+	j=len(lista)
 	if(comparador=='mayor'):
 		#recorre la lista y busca cual es el mayor
-		pass
+		for i in range(0,j):
+			pre=lista[i][2]
+			if pre>pre_m[2]:
+				pre_m[0]=lista[i][0]
+				pre_m[1]=lista[i][1]
+				pre_m[2]=pre
+		
 	elif(comparador=='menor'):
 		#recorre la lista y busca cual es el menor
-		pass
+		for i in range(0,j):
+			pre=lista[i][2]
+			if (pre<pre_m[2]) or (pre_m[2]==0):
+				pre_m[0]=lista[i][0]
+				pre_m[1]=lista[i][1]
+				pre_m[2]=pre	
 	else:
 		#error el comparador no es valido
 		pass
+	return pre_m
 
 
 def promedio_anual(anio):
 	#calcula el promedio anual
-	#usa lista precio
-	lista_t_p=lista_precios()#listado de todos los precios registrados
+	lista=obtener_datos_por_anio(anio)
 	t_pre=0#total de precio
-	for i in range(1,74):
-		a=lista_t_p[i][0]
-		pre=lista_t_p[i][2]
-		if anio==a:
-			t_pre+=pre
+	j=len(lista)
+	for i in range(0,j):
+		t_pre+=lista[i][2]
 	return t_pre/12 #devuelve el precio promedio que tuvo en un anio
 	
 
